158fe2d0cbfdd47ac43ff854efee7ff507b13f97
purpleeeee <h.purpleeeee@gmail.com>
2021-03-08 21:28:13 +0900

    🌱  Add WaitingModal

---
+++ src/components/WaitingModal.tsx
@@ -0,0 +1,34 @@
+import React, { VFC } from "react";
+import {
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Spinner,
+} from "@chakra-ui/react";
+
+type WaitingModalProps = {
+  onClose: () => void;
+  players: any[];
+};
+
+export const WaitingModal: VFC<WaitingModalProps> = ({ onClose, players }) => (
+  <Modal isOpen={true} onClose={onClose} isCentered closeOnOverlayClick={false}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalBody py="6" textAlign="center">
+        <Text>プレイヤーが4人揃うのを待っています...</Text>
+        <Text>{`${players.length}/4`}</Text>
+        <Spinner
+          mt="4"
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </ModalBody>
+    </ModalContent>
+  </Modal>
+);
